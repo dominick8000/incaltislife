df24c26d2bb944e03e1117333c7134813aa6a000
K3ating <christopher@rowlands.de>
2014-05-25 20:13:14 +0200

    Revert "EMP"
    This reverts commit f34ba1d6cdbd3082f6acf6a67f492cd7ce0fdd6c.

--- Altis_Life.Altis/Functions.h
+++ Altis_Life.Altis/Functions.h
@@ -377,18 +377,5 @@ class Life_Client_Core
         class calcGarageSpawn {};
         class onPut {};
         class onTake {};
-    };
-
-	class Special
-	
-	{
-		file = "core\special";
-		class openEmpMenu {};
-		class isEmpOperator {};
-		class scanVehicles {};
-		class warnVehicle {};
-		class empVehicle {};
-		class vehicleWarned {};
-		class vehicleEmpd {};
-	};
+    };	
 };
